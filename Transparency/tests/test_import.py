de298392c82685b45ce3f5ef1a9f87eb950c9991
KacperKubara <kjk1u17@soton.ac.uk>
2021-01-08 17:02:59 +0100

    Fresh fork of the repo

---
+++ Transparency/tests/test_import.py
@@ -0,0 +1,7 @@
+import Transparency
+from Transparency.model import *
+from Transparency.common_code import *
+from Transparency.Trainers import * 
+
+if __name__=="__main__":
+    print("Import test successful, wo-hoo")
