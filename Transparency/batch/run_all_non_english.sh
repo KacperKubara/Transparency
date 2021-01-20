a86d9d236e789c054d1fa8fdb4eb05df0b4a3a8f
KacperKubara <kjk1u17@soton.ac.uk>
2021-01-20 11:23:39 +0100

    run over all combinations 5 times

--- Transparency/batch/run_all_non_english.sh
+++ Transparency/batch/run_all_non_english.sh
@@ -20,13 +20,18 @@ models="vanilla_lstm ortho_lstm diversity_lstm"
 diversity_weight=0.5
 n_epochs=1
 
-for data in $datasets
-do 
-    output_path="./experiments/${data}"
-    for model in $models
-    do
-        echo $data $model
-        python train_and_run_experiments_bc.py --dataset ${data} --data_dir . --output_dir ${output_path} --encoder ${model} --diversity ${diversity_weight} --n_iter ${n_epochs}
+for i in {0..4}
+do
+    echo "RUN NUMBER: $i"
+    echo "============================================================\n\n\n\n"
+    for data in $datasets
+    do 
+        output_path="./experiments/${data}"
+        for model in $models
+        do
+            echo $data $model
+            python train_and_run_experiments_bc.py --dataset ${data} --data_dir . --output_dir ${output_path} --encoder ${model} --diversity ${diversity_weight} --n_iter ${n_epochs}
 
+        done
     done
 done
