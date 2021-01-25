dd664e7f9260e585d067f886e2680da96583193c
blazejmanczak <manczakb@gmail.com>
2021-01-25 16:10:43 +0100

    adapted argparse

--- Transparency/ExperimentsTransformer.py
+++ Transparency/ExperimentsTransformer.py
@@ -17,76 +17,89 @@ if __name__ == "__main__":
 
     parser = ArgumentParser()
 
-    parser.add_argument("-e", "--num-epochs",
-                        dest="num_epochs",
-                        help="Number of epochs.",
-                        default=10, type=int)
-
-    parser.add_argument("-b", "--batch-size",
-                        dest="batch_size",
-                        help="The batch size.",
-                        default=4, type=int)
-
-    parser.add_argument("-l", "--learn-rate",
-                        dest="lr",
-                        help="Learning rate",
-                        default=0.0001, type=float)
-
-    #parser.add_argument("-T", "--tb_dir", dest="tb_dir",
-    #                    help="Tensorboard logging directory",
-    #                    default='./runs')
-
-    parser.add_argument("--max_pool", dest="max_pool",
-                        help="Use max pooling in the final classification layer.",
-                        action="store_true")
-
-    parser.add_argument("-E", "--embedding", dest="embedding_size",
-                        help="Size of the character embeddings.",
-                        default=300, type=int) # 128
-
-    parser.add_argument("-M", "--max", dest="max_length",
-                        help="Max sequence length. Longer sequences are clipped (-1 for no limit).",
-                        default=512, type=int)
-
-    parser.add_argument("-H", "--heads", dest="num_heads",
-                        help="Number of attention heads.",
-                        default=8, type=int)
-
-    parser.add_argument("-d", "--depth", dest="depth",
-                        help="Depth of the network (nr. of self-attention layers/ transformer blocks)",
-                        default=1, type=int) # 6
-
-    parser.add_argument("-r", "--random-seed",
-                        dest="seed",
-                        help="RNG seed. Negative for random",
-                        default=1, type=int)
-
-    parser.add_argument("--gradient_clipping",
-                            dest="gradient_clipping",
-                            help="Gradient clipping.",
-                            default=1.0, type=float)
-
-    parser.add_argument("--diversity_transformer",
-                            help="Add diveristy term to loss?",
-                            default=False, type=bool)
-
-    parser.add_argument("--diversity_weight",
-                            help="Diversity weight for the diveristy part of the loss",
-                            default=0.5, type=float) 
-
-    parser.add_argument("--delete_prop",
-                            help="What proportion of the values with the heighest weight should be set to 0?",
-                            default=0, type=float) 
+    parser.add_argument("--model_type",
+                            choices= ["diversity_transformer", "vanilla_transformer"],
+                            required=True, type=str)
 
     parser.add_argument("--dataset_name",
                         help = "Name of the dataset to train on",
                         required = True,type = str)
+    parser.add_argument("--random-seed", help = "What random seed to use?",
+                        default = 0, type = int)
+    
 
-    parser.add_argument("--lr-warmup",
-                    dest="lr_warmup",
-                    help="Learning rate warmup.",
-                    default=5_000, type=int)
-                        
     arg = parser.parse_args()
     print("Options:", arg)
     run_experiments(arg)
+
+"""
+parser.add_argument("-e", "--num-epochs",
+                dest="num_epochs",
+                help="Number of epochs.",
+                default=10, type=int)
+
+parser.add_argument("-b", "--batch-size",
+                dest="batch_size",
+                help="The batch size.",
+                default=4, type=int)
+
+parser.add_argument("-l", "--learn-rate",
+                dest="lr",
+                help="Learning rate",
+                default=0.0001, type=float)
+
+#parser.add_argument("-T", "--tb_dir", dest="tb_dir",
+#                    help="Tensorboard logging directory",
+#                    default='./runs')
+
+parser.add_argument("--max_pool", dest="max_pool",
+                help="Use max pooling in the final classification layer.",
+                action="store_true")
+
+parser.add_argument("-E", "--embedding", dest="embedding_size",
+                help="Size of the character embeddings.",
+                default=300, type=int) # 128
+
+parser.add_argument("-M", "--max", dest="max_length",
+                help="Max sequence length. Longer sequences are clipped (-1 for no limit).",
+                default=512, type=int)
+
+parser.add_argument("-H", "--heads", dest="num_heads",
+                help="Number of attention heads.",
+                default=8, type=int)
+
+parser.add_argument("-d", "--depth", dest="depth",
+                help="Depth of the network (nr. of self-attention layers/ transformer blocks)",
+                default=1, type=int) # 6
+
+parser.add_argument("-r", "--random-seed",
+                dest="seed",
+                help="RNG seed. Negative for random",
+                default=1, type=int)
+
+parser.add_argument("--gradient_clipping",
+                    dest="gradient_clipping",
+                    help="Gradient clipping.",
+                    default=1.0, type=float)
+
+parser.add_argument("--diversity_transformer",
+                    help="Add diveristy term to loss?",
+                    default=False, type=bool)
+
+parser.add_argument("--diversity_weight",
+                    help="Diversity weight for the diveristy part of the loss",
+                    default=0.5, type=float) 
+
+parser.add_argument("--delete_prop",
+                    help="What proportion of the values with the heighest weight should be set to 0?",
+                    default=0, type=float) 
+
+parser.add_argument("--dataset_name",
+                help = "Name of the dataset to train on",
+                required = True,type = str)
+
+parser.add_argument("--lr-warmup",
+            dest="lr_warmup",
+            help="Learning rate warmup.",
+            default=5_000, type=int)
+"""       
--- Transparency/Trainers/TrainerTransformer.py
+++ Transparency/Trainers/TrainerTransformer.py
@@ -13,20 +13,23 @@ from argparse import ArgumentParser
 import random, tqdm, sys, math, gzip
 import os
 from Transparency.model.transformerUtils import get_curr_time, delete_weights, _conicity, d, eval_acc, get_conicity_mask
-from Transparency.model.transformerDataHandling import vectorize_data, datasets
+from Transparency.model.transformerDataHandling import vectorize_data, datasets, dataset_config
 from Transparency.model.modules.transformer import CTransformer
 import pickle
 import time
 
-def go(arg):
+def go(config):
     """
     Creates and trains a basic transformer for the sentiment classification task.
     """
     
     start_time = time.time()
 
-    #arg = some func
-    logging_path = os.path.join("experiments", arg.dataset_name, "_diversity_transformer_" + str(arg.diversity_transformer), f'experiment_{get_curr_time()}.txt')
+    arg = dataset_config[config.dataset_name]["arg"]
+    dataset_config[config.dataset_name]["arg"].diversity_transformer = True if config.model_type=="diversity_transformer" else False
+    print("Diversity",  dataset_config[config.dataset_name]["arg"].diversity_transformer ) 
+
+    logging_path = os.path.join("experiments", arg.dataset_name, config.model_type, f'experiment_{get_curr_time()}.txt')
 
     if not os.path.exists(logging_path):
         os.makedirs(logging_path)
@@ -49,9 +52,10 @@ def go(arg):
     dataset_args['padding_length'] = arg.max_length
     dataset_args['padding_token']  = 0
     dataset_args['batch_size'] = arg.batch_size
-
+    
     #vec = vectorize_data(os.path.join("preprocess", arg.dataset_name.upper(), "{}_dataset.csv".format(arg.dataset_name)), min_df=1) 
     print(os.path.join("preprocess", arg.dataset_name.upper(), "vec_{}.p".format(arg.dataset_name)) )
+    
 
     #vec = pickle.load(open(os.path.join("preprocess", arg.dataset_name.upper(), "vec_{}.p".format(arg.dataset_name)), "rb") )
     vec = pickle.load(open("preprocess/20News/vec_20news_sports.p", "rb") )
@@ -66,7 +70,7 @@ def go(arg):
 
     # create the model
     model = CTransformer(emb=arg.embedding_size, heads=arg.num_heads, depth=arg.depth, seq_length=mx, num_tokens=vec.vocab_size,
-     num_classes=NUM_CLS, max_pool=arg.max_pool, delete_prop=arg.delete_prop)
+     num_classes=NUM_CLS)
 
     model.token_embedding.weight.data.copy_(torch.from_numpy(vec.embeddings))
     model.token_embedding.weight.requires_grad = False
@@ -109,7 +113,7 @@ def go(arg):
             #tbw.add_scalar("mean_batch_conicity", mean_batch_conicity, (e+1)*(i+1))
 
             # DIVERSITY COMPOMNENT
-            if arg.diversity_transformer == True:
+            if arg.diversity_transformer == "diversity_transformer":
               loss = nll + arg.diversity_weight*mean_batch_conicity
             else: 
               loss = nll
@@ -131,22 +135,22 @@ def go(arg):
         with torch.no_grad():
             
             acc = eval_acc(model, dataset.dev_data)
-            if acc > best_acc:
-                best_acc = acc
-                print("new best valiadtion achieved: {}. Saving model ...".format(best_acc))
-                torch.save({
-                'epoch': e,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': opt.state_dict(),
-                'loss': loss,
-                }, SAVE_PATH.format(round(acc,3)))
-
             log_file.write(f'-- {"validation"} accuracy {acc:.3}')
             print(f'-- {"validation"} accuracy {acc:.3}')
             mean_epoch_conicity = torch.mean(torch.stack(mean_conicity_values))
             print("Mean concity value {}".format(mean_epoch_conicity))
             log_file.write(f'-- mean conicity in epoch {mean_epoch_conicity:.4}')
             log_file.write(f'-- loss  {float(loss.item()):.3}')
+            if acc > best_acc:
+                best_acc = acc
+                print("new best valiadtion accuracy achieved:. Saving model ...")
+                torch.save({
+                'epoch': e,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': opt.state_dict(),
+                'loss': loss,
+                }, SAVE_PATH.format(round(acc,3)))
+                log_file.write("Saving model with best validation accuracy so far...")
             # Upon epoch completion, write to tensoraboard
             #tbw.add_scalar('mean_epoch_conicity', mean_epoch_conicity, e)
             #tbw.add_scalar('classification/test-loss', float(loss.item()), e)
@@ -158,3 +162,6 @@ def go(arg):
     best_model.to(device)
     test_acc = eval_acc(best_model, dataset.test_data)
     print(f"Training concluded. Best model with validation accuracy {best_acc:.3} achieved test accuracy: {test_acc}")
+    total_time = time.time() - start_time
+    print(f"Execution took {total_time:.3} seconds")
+    log_file.write(f"\n total time {total_time:.3} [seconds]")
