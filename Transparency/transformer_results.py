a65afbaeb7b4fc887d6059ccec546f145153af3d
blazejmanczak <manczakb@gmail.com>
2021-01-27 00:40:04 +0100

    testintg the experiments script

--- Transparency/transformer_results.py
+++ Transparency/transformer_results.py
@@ -9,17 +9,15 @@ def gather_stats(arg):
     log_file = open("god_script_transformer", "w+" )
     options = ["diversity_transformer", "vanilla_transformer"]
     
-    #god_dic = {}
-
     for d in datasets.keys():
-        #god_dic[d] = {}
+
         print(f"\n Working on the dataset {d}")
         for opt in options:
             test_accs = []
             conicities = []
             print(f"\n Running model type {opt}")
             for i in range(arg.num_runs):
-                #god_dic[d]["run"] = {}
+                print(f'\n Dataset {d}, model type {opt}, run {i+1}')
                 run_params = easydict.EasyDict({"model_type":opt,"dataset_name": d})
                 best_acc, test_acc, mean_epoch_conicity = run_experiments(run_params)
                 
@@ -28,11 +26,11 @@ def gather_stats(arg):
 
                 log_file.write(f'\n Dataset {d}, model type {opt}, run {i+1}')
                 log_file.write(f'    \n Val_acc {best_acc}, test_acc {test_acc}, conicity {mean_epoch_conicity}')
-                print(f'Dataset {d}, model type {opt}, run {i+1}')
-                print(f'Val_acc {best_acc}, test_acc {test_acc}, conicity {mean_epoch_conicity}')
+                
+                print(f'\n Val_acc {best_acc}, test_acc {test_acc}, conicity {mean_epoch_conicity}')
 
-            log_file.write(f'\n       Mean test accuracies {np.mean(test_accs)}, std {np.std(test_accs)}')
-            log_file.write(f'\n       Mean conicities {np.mean(conicities)}, std {np.std(conicities)}')
+            log_file.write(f'\n Mean test accuracies {np.mean(test_accs)}, std {np.std(test_accs)}')
+            log_file.write(f'\n Mean conicities {np.mean(conicities)}, std {np.std(conicities)}')
             print(f'Mean test accuracies {np.mean(test_accs)}, std {np.std(test_accs)}')
             print(f'Mean conicities {np.mean(conicities)}, std {np.std(conicities)}')
 
